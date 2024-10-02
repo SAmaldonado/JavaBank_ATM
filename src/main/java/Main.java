1c420cc19803c5b7fac1b8fb7b68ab44e1bb12d1
SAmaldonado <samaldonado@unal.edu.co>
2024-10-01 21:42:41 -0500

    Initial!: Begin the developent of the ATM machine
    BREAKING CHANGE: Its the first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to JavaBank ATM! Version control with Git.");
+    //Create a Scanner instance
+    Scanner scanner = new Scanner(System.in);
+
+    //Print the greeting
+    System.out.println("Por favor ingrese su usario");
+
+    //Print and Scan the sing-in fields
+    System.out.println("Usuario: ");
+    String user = scanner.nextLine();
+    System.out.println("Contraseña: ");
+    String pin = scanner.nextLine();
+
     }
 }
